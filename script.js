e678b613ae56802b5b48ba7bfb172da9316c3aa2
unknown <kandagatlakoushal@gmail.com>
2022-05-03 17:48:31 +0530

    popup added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//!dark/Light Mode
 const darkModeToggle=document.querySelector('#dark-mode-toggle');
 
 
@@ -6,3 +6,15 @@ darkModeToggle.addEventListener('click',()=>{
     document.body.classList.toggle('darkmode')
 
 }) 
+
+//! Popup-Window
+function popupWindow(){
+    document.querySelector('#popup').style.display='flex';
+
+}
+let popupCloseButton=document.querySelector('#popupClose')
+console.log(popupCloseButton)
+popupCloseButton.addEventListener('click',()=>{
+    console.log('clicked')
+    document.querySelector('#popup').style.display='none';
+})
